a48674828f423d726f32bf027793497deef26b20
Lil-Kr <cxqybox@163.com>
2024-10-05 23:38:39 +0800

    binary tree

---
+++ binary-tree/src/test/java/com/cy/leetcodeplay/question/no129/SolutionTest.java
@@ -0,0 +1,15 @@
+package com.cy.leetcodeplay.question.no129;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+
+	private Solution solution = new Solution();
+
+	@Test
+	public void test1() {
+
+	}
+}
---
+++ binary-tree/src/test/java/com/cy/leetcodeplay/question/no257/SolutionTest.java
@@ -0,0 +1,12 @@
+package com.cy.leetcodeplay.question.no257;
+
+import org.junit.jupiter.api.Test;
+
+class SolutionTest {
+
+	private Solution solution = new Solution();
+
+	@Test
+	public void test1() {
+	}
+}
