9171119b00382f2b840d269510c08b6c839decbe
Lil-Kr <cxqybox@163.com>
2024-12-07 21:30:38 +0800

    2D dp

--- dp/src/test/java/com/cy/leetcodeplay/dp/no1143/SolutionTest.java
+++ dp/src/test/java/com/cy/leetcodeplay/dp/no1143/SolutionTest.java
@@ -14,4 +14,32 @@ class SolutionTest {
 		int res1 = solution.longestCommonSubsequence(text1, text2);
 		Assertions.assertEquals(3, res1);
 	}
+
+	@Test
+	public void test2() {
+		String text1 = "abcde", text2 = "ace";
+		int res1 = solution.longestCommonSubsequence2(text1, text2);
+		Assertions.assertEquals(3, res1);
+	}
+
+	@Test
+	public void test3() {
+		String text1 = "abcde", text2 = "ace";
+		int res1 = solution.longestCommonSubsequence3(text1, text2);
+		Assertions.assertEquals(3, res1);
+	}
+
+	@Test
+	public void test4() {
+		String text1 = "abcde", text2 = "ace";
+		int res1 = solution.longestCommonSubsequence4(text1, text2);
+		Assertions.assertEquals(3, res1);
+	}
+
+	@Test
+	public void test5() {
+		String text1 = "abcde", text2 = "ace";
+		int res1 = solution.longestCommonSubsequence5(text1, text2);
+		Assertions.assertEquals(3, res1);
+	}
 }
