87fefebb33880f4087847e51ebb06fa02a3fb59d
Lil-Kr <cxqybox@163.com>
2025-03-01 22:02:07 +0800

    leetcode no.54

--- matrix/src/main/java/com/cy/leetcodeplay/matrix/no54/Solution.java
+++ matrix/src/main/java/com/cy/leetcodeplay/matrix/no54/Solution.java
@@ -1,5 +1,6 @@
 package com.cy.leetcodeplay.matrix.no54;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -12,7 +13,41 @@ public class Solution {
 
 
 	public List<Integer> spiralOrder(int[][] matrix) {
+		List<Integer> res = new ArrayList<>();
+		if (matrix.length == 0) return res;
 
-		return null;
+		int top = 0, bottom = matrix.length - 1;
+		int left = 0, right = matrix[0].length - 1;
+
+		while (top <= bottom && left <= right) {
+			// 从左到右遍历 top 行
+			for (int i = left; i <= right; i++) {
+				res.add(matrix[top][i]);
+			}
+			top++;
+
+			// 从上到下遍历 right 列
+			for (int i = top; i <= bottom; i++) {
+				res.add(matrix[i][right]);
+			}
+			right--;
+
+			if (top <= bottom) {
+				// 从右到左遍历 bottom 行
+				for (int i = right; i >= left; i--) {
+					res.add(matrix[bottom][i]);
+				}
+				bottom--;
+			}
+
+			if (left <= right) {
+				// 从下到上遍历 left 列
+				for (int i = bottom; i >= top; i--) {
+					res.add(matrix[i][left]);
+				}
+				left++;
+			}
+		}
+		return res;
 	}
 }
--- matrix/src/test/java/com/cy/leetcodeplay/matrix/no54/SolutionTest.java
+++ matrix/src/test/java/com/cy/leetcodeplay/matrix/no54/SolutionTest.java
@@ -1,14 +1,25 @@
 package com.cy.leetcodeplay.matrix.no54;
 
+import com.alibaba.fastjson2.JSONArray;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
 
 class SolutionTest {
 	private Solution solution = new Solution();
 
 	@Test
 	public void test1() {
-		solution.spiralOrder(null);
+		int[][] matrix = {{1,2,3},{4,5,6},{7,8,9}};
+		List<Integer> res = solution.spiralOrder(matrix);
+		Assertions.assertEquals("[1,2,3,6,9,8,7,4,5]", JSONArray.toJSONString(res));
+	}
+
+	@Test
+	public void test2() {
+		int[][] matrix = {{1,2,3,4},{5,6,7,8},{9,10,11,12}};
+		List<Integer> res = solution.spiralOrder(matrix);
+		Assertions.assertEquals("[1,2,3,4,8,12,11,10,9,5,6,7]", JSONArray.toJSONString(res));
 	}
 }
