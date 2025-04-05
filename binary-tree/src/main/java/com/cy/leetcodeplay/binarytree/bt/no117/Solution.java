1b532f4ff42a34998711c406ad0a5a538044b1cf
Lil-Kr <cxqybox@163.com>
2025-04-05 11:26:21 +0800

    leetcode no.117

--- binary-tree/src/main/java/com/cy/leetcodeplay/binarytree/bt/no117/Solution.java
+++ binary-tree/src/main/java/com/cy/leetcodeplay/binarytree/bt/no117/Solution.java
@@ -3,6 +3,9 @@ package com.cy.leetcodeplay.binarytree.bt.no117;
 
 import com.cy.leetcodeplay.common.entity.Node;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * @Author: Lil-K
  * @Date: 2025/4/5
@@ -11,8 +14,36 @@ import com.cy.leetcodeplay.common.entity.Node;
  */
 public class Solution {
 
-
+	/**
+	 * 与 116 不同的是, no.117 不是完全二叉树, 是不平衡的二叉树, 不能直接遍历
+	 * @param root
+	 * @return
+	 */
 	public Node connect(Node root) {
+		if (root == null) return null;
+
+		Queue<Node> queue = new LinkedList<>();
+		queue.offer(root);
+		while (!queue.isEmpty()) {
+			int size = queue.size();
+			Node prev = null;
+			for (int i = 0; i < size; i++) {
+				Node cur = queue.poll();
+
+				if (prev != null) {
+					prev.next = cur;
+				}
+				prev = cur;
+
+				if(cur.left != null) {
+					queue.offer(cur.left);
+				}
+
+				if(cur.right != null) {
+					queue.offer(cur.right);
+				}
+			}
+		}
 
 		return root;
 	}
